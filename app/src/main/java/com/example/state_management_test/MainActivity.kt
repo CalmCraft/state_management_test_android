d212bf1e5f48f2c12ae8f81749f61200fe0ae807
Aung Pyae <aungpyae2387@gmail.com>
2023-07-25 12:59:52 +0630

    initialize

---
+++ app/src/main/java/com/example/state_management_test/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.state_management_test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.state_management_test.ui.theme.State_management_testTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            State_management_testTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BGLayer()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun BGLayer() {
+    Image(painter = painterResource(id = R.drawable.welcome), contentDescription = "", contentScale = ContentScale.Crop)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    State_management_testTheme {
+        Greeting("Android")
+    }
+}
